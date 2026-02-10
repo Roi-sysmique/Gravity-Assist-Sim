6c95b2e030ca15afd940f4f3fe172169e9a0bd86
Hamza <elhechmihamza@gmail.com>
2026-02-10 21:05:59 +0100

    Created Entity Object Class

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-from os import fspath
-
 import pygame
 import sys
+from pygame import Vector2
 
 pygame.init()
 
@@ -11,12 +10,26 @@ pygame.display.set_caption("Gravity Assist Sim")
 clock = pygame.time.Clock()
 
 class Entity(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, position:Vector2, mass:float, velocity:Vector2, radius:int, color:tuple):
         pygame.sprite.Sprite.__init__(self)
-        pass
+        self.mass = mass
+        self.position = position
+        self.velocity = velocity
+        self.radius = radius
+        self.diameter = radius * 2
+        self.image = pygame.Surface((self.diameter, self.diameter)).convert_alpha()
+        self.image.fill('white')
+        self.image.set_colorkey('white')
+        self.rect = self.image.get_rect(center=self.position)
+        self.color = color
+        
+    def draw(self, surface):
+        surface.blit(self.image, self.rect)
+        pygame.draw.circle(self.image, self.color, (self.radius, self.radius), self.radius)
 
 def main():
     fps = 60
+    entity = Entity(Vector2(WIDTH / 2, HEIGHT / 2), 0, Vector2(50, 50), 20, (255,255,0))
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -24,6 +37,7 @@ def main():
                 sys.exit()
 
         SCREEN.fill('light blue')
+        entity.draw(SCREEN)
         pygame.display.update()
         clock.tick(fps)
 
